87be2477eba0d90d29f5c79c893cae039ac07d5f
unknown <nontsuke.ken@gmail.com>
2022-03-21 19:56:55 -0700

    new project temp

---
+++ New_Floating_Island_Escape/Assets/Script/Climber.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Climber : MonoBehaviour
+{
+
+    private CharacterController character;
+    public  static XRController climbingHand;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (climbingHand)
+        {
+            
+            Climb(); 
+        }
+    
+    }
+
+    //Calculate Climb physic
+    void Climb()
+    {
+        InputDevices.GetDeviceAtXRNode(climbingHand.controllerNode).TryGetFeatureValue(CommonUsages.deviceVelocity, out Vector3 velocity);
+
+        character.Move( transform.rotation * -velocity * Time.fixedDeltaTime);
+    }
+}
